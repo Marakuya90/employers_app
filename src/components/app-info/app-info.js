a33b78fee276fc4a65907d9d4358e629f63d9f73
Marakuya90 <zhilzhil@mail.ru>
2023-03-09 14:14:47 +0300

    make components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников компании N</h1>
+            <h2>Общее число сотрудников: </h2>
+            <h1>Премию получат: </h1>
+        </div>
+    )
+}
+
+export default AppInfo;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,23 @@
-
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForms from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
 function App() {
     return (
         <div className="app" >
+            <AppInfo />
 
+            <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+            </div>
+
+            <EmployersList/>
+            <EmployersAddForms/>
         </div>
     )
 }
